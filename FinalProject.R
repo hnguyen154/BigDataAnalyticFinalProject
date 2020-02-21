7f00ec4538ad915f63601faae2a177474188148c
Hao Nguyen <honghao97@gmail.com>
2020-02-21 11:29:20 -0500

    SVM added

--- FinalProject.R
+++ FinalProject.R
@@ -15,6 +15,10 @@ library(DMwR)    # For KNN
 library(ggvis)
 library(dplyr)
 library(randomForest)
+# install.packages("devtools")
+library(devtools)
+#devtools::install_github('araastat/reprtree')
+library(reprtree)
 
 ##---------------------------------IMPORT DATA---------------------------------
 #Original dataset has splitted by 70% training and 30% testing
@@ -49,6 +53,7 @@ sum(is.na(y_test))
 class_train = y_train[,1]
 class_test = y_test[,1]
 
+#------1st train---------
 ## A 5-nearest neighbors model with no normalization
 knn5_pred <- knn(train = X_train, test = X_test, cl = class_train, k=5)
 #NROW(knn5_pred) # compare size with y_test
@@ -56,14 +61,14 @@ knn5_pred <- knn(train = X_train, test = X_test, cl = class_train, k=5)
 table(knn5_pred , class_test)
 confusionMatrix(table(knn5_pred , class_test))
 
-
+#------2nd train---------
 ## A 10-nearest neighbors model with no normalization
 knn10_pred <- knn(train = X_train, test = X_test, cl = class_train, k=10)
 ##Evaluation KNN_10
 table(knn10_pred , class_test)
 confusionMatrix(table(knn10_pred , class_test))
 
-
+#------3rd train---------
 ## A 25-nearest neighbors model with no normalization
 knn25_pred <- knn(train = X_train, test = X_test, cl = class_train, k=25)
 ##Evaluation KNN_25
@@ -72,11 +77,15 @@ confusionMatrix(table(knn25_pred , class_test))
 
 # *** Not such different in change in K-neighbor 5/10/25
 # *** Class 3 and 4 did not have a google accuracy. Best classes are 1 and 6.
+#Accuracy ~ 90%
 
 #---------------------------------Random Forest---------------------------------
+#------1st train---------
 data.rf=randomForest(class_train ~ ., data=X_train, ntree=100, mtry=2, importance=TRUE)
 data.rf #Confustion matrix
 varImpPlot(data.rf)
+## Mean Decrease in Accuracy - The decrease in accuracy as a result of this permuting is averaged over all trees, and is used as a measure of the importance of variable j in the random forest. 
+## Mean Decrease in Gini -  calculates each feature importance as the sum over the number of splits (across all tress) that include the feature, proportionally to the number of samples it splits.
 
 #Predicting using random forest model
 X_test$pred_rf<-predict(object = data.rf,X_test)
@@ -84,13 +93,37 @@ X_test$pred_rf.prob<-predict(object = data.rf,X_test,type="prob")
 
 #Checking the accuracy of the random forest model
 confusionMatrix(class_test,X_test$pred_rf)
+#Accuracy ~ 92%
 
-# use upliftRF to apply a Random Forest for uplift modeling
-up.fit.RF <- upliftRF(MOVED_AD_NUM ~ AGE + NH_WHITE + COMM_PT + H_F1 + REG_DAYS+ 
-                        PR_PELIG + E_PELIG + POLITICALC  + trt(MESSAGE_A),
-                      data = train.df, mtry = 3, ntree = 100, split_method = "KL",
-                      minsplit = 200, verbose = TRUE)
-pred <- predict(up.fit.RF, newdata = valid.df)
+#Print Tree
+#reprtree:::plot.getTree(data.rf)
+#tree <- getTree(data.rf, k=1, labelVar=TRUE)
+#realtree <- reprtree:::as.tree(tree, data.rf)
+
+#------2nd train---------
+#Train 200 Trees and do.trace=100
+data.rf2=randomForest(class_train ~ ., data=X_train, ntree=200, mtry=2,do.trace=100, importance=TRUE)
+data.rf2 #Confustion matrix
+varImpPlot(data.rf2)
+
+#Predicting using random forest model
+X_test$pred_rf2<-predict(object = data.rf2,X_test)
+X_test$pred_rf.prob2<-predict(object = data.rf2,X_test,type="prob")
+
+#Checking the accuracy of the random forest model
+confusionMatrix(class_test,X_test$pred_rf2)
+
+#Accuracy ~ 92%
+
+#---------------------------------------SVM-------------------------------------
+# Fitting model
+fit <-svm(class_train ~ ., data = X_train)
+summary(fit)
+#Predict Output 
+predicted= predict(fit,X_test)
+confusionMatrix(class_test,predicted)
+
+#Accuracy ~ 95%
 
 #--------------------------------Neutral Network--------------------------------
 
